fb744db5f169a73336b35f9102c44f1b6cbaf871
javajava2 <mross.piotr@gmail.com>
2018-06-03 12:23:03 +0200

    second commit

--- src/main/java/skaner/Main.java
+++ src/main/java/skaner/Main.java
@@ -1,52 +1,28 @@
 package skaner;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
-    String directory;
-
-
     public static void main(String args[]) throws FileNotFoundException {
-        int choice;
-
-
-    public void setDirectory(String directory) {
-        this.directory = directory;
 
     Scanner input = new Scanner(System.in);
 
         System.out.println("Podaj sciezke katalogu");
-        directory = input.nextLine();
+        sourceDirectory = input.nextLine();
 
-//                "by sprawdzic jego rozmiar oraz rozmiar wszystkich plików, " +
-//                "w którym dany plik/katalog się znajduje (razem z podfolderami w dół) oraz wylistuje na ekranie te pliki, " +
-//                "których rozmiar przekracza 10MB i " +
-//                "ostatnia data modyfikacji jest ponad miesiąc temu" +
-//                        " \n 2 - Wyswietlenie listy plikow w podanym katlaogu\n 3 - Usuwanie plikow z podanego katalogu\n");
+        System.out.println("Wybierz operacje:");
+        System.out.println("1 - Sprawdz rozmiar katalogu oraz rozmiar wszystkich plików");
+        System.out.println("oraz wylistuj na ekranie te pliki, których rozmiar przekracza 10MB i ostatnia data modyfikacji jest ponad miesiąc temu");
+        System.out.println("2 - Wyswietlenie listy plikow w podanym katalogu");
+        System.out.println("3 - Usuwanie plikow z podanego katalogu");
 
 
-    choice =input.nextInt();
-        switch(choice)
 
-    {
+        int choice =input.nextInt();
+        switch(choice) {
         case 1:
-
-            File[] fileArray = new File(directory).listFiles();
-
-            for (File f : fileArray) // loop thru all files
-            {
-
-                if (f.getName().endsWith(".txt")) // to deal with the .txt files.
-                {
-                    try (Scanner s = new Scanner(f)) {
-                    }
-
-                }
-
-            }
             System.out.println();
             break;
         case 2:
